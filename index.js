89e8a6a558414e195e91fd30fa2bd1a7567e85cf
Olibt1107 (Alt) <admin@voidium.uk>
2026-06-10 12:16:07 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
+const path = require("path");
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+const app = express();
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+app.use(express.static("public"));
+
+app.listen(3000, () => {
+    console.log("Running");
+});
